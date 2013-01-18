928af1e300a1a0565092cd2c4279b02e8b04b998
Dmitrii Kondratev <dimonloboto@gmail.com>
2013-01-18 10:18:17 -0800

    Create Prog1_1.cpp

---
+++ C#/Prog1_1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void variant1();
+void variant2();
+void variant3();
+
+int main()
+{
+  variant1();
+	variant2();	
+	variant3();
+	System("PAUSE");
+`	return 0;	
+}
+
+void variant1()
+{
+	cout << "1 2 3 4" << endl;
+}
+void variant2()
+{
+	cout << "1 " << "2 " << "3 " << "4" << endl;
+}
+void variant3()
+{
+	cout << "1 ";
+	cout << "2 ";
+	cout << "3 ";
+	cout << "4" << endl;
+}
